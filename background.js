8653837b304b410413d055ecc79b3f5d89e020d3
Wendy <tell.wendy@gmail.com>
2019-02-24 02:17:36 -0800

    roboSitter chrome extension

---
+++ background.js
@@ -0,0 +1,85 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+const whiteList = [
+  {label: 'Baby Shark Dance | Sing and Dance! | Animal Songs | PINKFONG Songs for Children', url: 'https://www.youtube.com/watch?v=XqZsoesa55w'},
+  {label: 'Sesame Street: If You\'re Happy and You Know It | Elmo\'s Sing-Along', url: 'https://www.youtube.com/watch?v=5015skRvqs8'},
+  {label: 'If You’re Happy and You Know It + More Baby Songs by Dave and Ava', url: 'https://www.youtube.com/watch?v=WbUP2PO9gFI'}
+]
+
+chrome.runtime.onInstalled.addListener(function() {
+  
+  chrome.storage.sync.set({ whiteList }, function() {
+    console.log("storing white list")
+  })
+  chrome.tabs.query({active: true, currentWindow: true}, tab => {
+    
+    if (!tab[0] || tab[0].url.indexOf("localhost") > -1 || tab[0].url.indexOf("github") > -1) {
+      return;
+    } 
+
+    // check if url is on whitelist, tab.url
+    if (tab[0].url.indexOf("chrome://") === -1) {
+      
+      const isOnList = whiteList.reduce((acc, item) => {
+        return acc || item.url === tab[0].url;
+      }, false);
+
+      if (!isOnList) {
+        chrome.tabs.executeScript({
+          code: "document.body.innerText='YOU DO NOT HAVE PERMISSION'",
+        }, function() {
+          if(chrome.runtime.lastError) {
+            console.error("Script injection failed: " + chrome.runtime.lastError.message);
+          }
+        })
+      }
+    }
+
+  })
+})
+
+
+chrome.tabs.onUpdated.addListener(function() {
+  
+  chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
+
+    if (!tab[0] || tab[0].url.indexOf("localhost") > -1 || tab[0].url.indexOf("github") > -1) {
+      return;
+    } 
+    
+    if (tab[0].url.indexOf("chrome://") === -1) {
+      console.log("falling in here")
+        // check if URL is on white list
+        const isOnList = whiteList.reduce((acc, item) => {
+            return acc || item.url === tab[0].url;
+          }, false);
+
+          console.log("isOnList", isOnList)
+        if (!isOnList) {
+          
+          chrome.tabs.executeScript({
+            code: "document.body.innerText='YOU DO NOT HAVE PERMISSION'",
+          }, function() {
+            if(chrome.runtime.lastError) {
+              console.error("Script injection failed: " + chrome.runtime.lastError.message);
+            }
+          })
+        }
+
+    }
+  })
+})
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {hostEquals: 'developer.chrome.com'},
+    })
+    ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
+});
---
+++ content.js
@@ -0,0 +1,5 @@
+console.log('content.js');
+// const body = document.body;
+
+// console.log("body>>>>>>>>>>>>>", body);
+// body.innerHTML('NOPE!')
